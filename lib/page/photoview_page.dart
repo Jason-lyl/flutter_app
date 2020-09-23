7e61c85e37c0bd5e5363384e82a0a4c9980932f2
liyulong <liyl@eagersoft.cn>
2020-09-23 14:20:44 +0800

    欢迎页面

--- lib/page/photoview_page.dart
+++ lib/page/photoview_page.dart
@@ -1,15 +1,68 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_app/common/style/gsy_style.dart';
+import 'package:flutter_app/common/utils/common_utils.dart';
+import 'package:flutter_app/widget/gsy_common_option_widget.dart';
+import 'package:flutter_app/widget/gsy_title_bar.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:photo_view/photo_view.dart';
 
 /**
  * @author: Jason
  * @create_at: Sep 16, 2020
  */
 
-@Deprecated("待完善")
-class PhotoViewPage extends StatefulWidget {
+class PhotoViewPage extends StatelessWidget {
   static const String sName = "PhotoViewPage";
 
   PhotoViewPage();
 
-  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+  @override
+  Widget build(BuildContext context) {
+    final String url = ModalRoute.of(context).settings.arguments;
+    return new Scaffold(
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(Icons.file_download),
+        onPressed: () {
+          CommonUtils.saveImage(url).then((res) {
+            if (res != null) {
+              Fluttertoast.showToast(msg: res);
+              if (Platform.isAndroid) {
+                const updataAlbum = const MethodChannel(
+                    'com.shuyu.gsygithub.gsygithubflutter/UpdateAlbumPlugin');
+                updataAlbum.invokeMethod('updateAlbum', {
+                  'path': res,
+                  'name': CommonUtils.splitFileNameByPath(res)
+                });
+              }
+            }
+          });
+        },
+      ),
+      appBar: new AppBar(
+        title: GSYTitleBar("", rightWidget: new GSYCommonOptionWidget(url: url)),
+      ),
+      body: new Container(
+        color: Colors.black,
+        child: new PhotoView(
+          imageProvider: new NetworkImage(url ?? GSYICons.DEFAULT_REMOTE_PIC),
+          loadingChild: Container(
+            child: new Stack(
+              children: <Widget>[
+                new Center(
+                  child: new Image.asset(GSYICons.DEFAULT_IMAGE,
+                  height: 180.0, width: 180.0)),
+                  new Center(
+                    child: new SpinKitFoldingCube(
+                      color: Colors.white30, size: 60.0))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
--- lib/page/welcome_page.dart
+++ lib/page/welcome_page.dart
@@ -1,26 +1,26 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/common/dao/user_dao.dart';
+import 'package:flutter_app/common/style/gsy_style.dart';
+import 'package:flutter_app/common/utils/navigator_utils.dart';
 import 'package:flutter_app/redux/gsy_state.dart';
+import 'package:flutter_app/widget/diff_scale_text.dart';
+import 'package:flutter_app/widget/mole_widget.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'dart:async';
 import 'package:redux/redux.dart';
-
-
 
 /**
  * @author: Jason
  * @create_at: Sep 16, 2020
  */
 
-@Deprecated("待完善")
 class WelecomePage extends StatefulWidget {
   static final String sName = "/";
 
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
+@override
+  _WelecomePageState createState() => _WelecomePageState();
 }
 
 class _WelecomePageState extends State<WelecomePage> {
@@ -30,7 +30,6 @@ class _WelecomePageState extends State<WelecomePage> {
 
   @override
   void didChangeDependencies() {
-    // TODO: implement didChangeDependencies
     super.didChangeDependencies();
     if (hadInit) {
       return;
@@ -54,16 +53,59 @@ class _WelecomePageState extends State<WelecomePage> {
     new Future.delayed(const Duration(seconds: 2, microseconds: 500), () {
       UserDao.initUserInfo(store).then((res) {
         if (res != null && res.result) {
-
+          NavigatorUtils.goHome(context);
         } else {
+          NavigatorUtils.goLogin(context);
         }
+        return true;
       });
     });
   }
 
   @override
   Widget build(Object context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return StoreBuilder<GSYState>(
+      builder: (context, store) {
+        double size = 20;
+        return Material(
+          child: new Container(
+            color: GSYColors.white,
+            child: Stack(
+              children: <Widget>[
+                new Center(
+                  child: new Image(
+                      image: new AssetImage('static/images/welcome.png')),
+                ),
+                Align(
+                  alignment: Alignment(0.0, 0.3),
+                  child: DiffScaleText(
+                    text: text,
+                    textStyle: GoogleFonts.akronim().copyWith(
+                      color: GSYColors.primaryValue,
+                      fontSize: fontSize,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment(0.0, 0.8),
+                  child: Mole(),
+                ),
+                new Align(
+                  alignment: Alignment.bottomCenter,
+                  child: new Container(
+                    width: size,
+                    height: size,
+                    child: new FlareActor("static/file/flare_flutter_logo_.flr",
+                        alignment: Alignment.topCenter,
+                        fit: BoxFit.fill,
+                        animation: "Placeholder"),
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
